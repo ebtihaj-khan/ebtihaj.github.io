53578553a593f0e7bde2624563ad046524a8406a
ebtihaj-khan <ebtihaj_316@yahoo.com>
2025-04-11 11:05:38 +0500

    Configure for GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
+  app: {
+    baseURL: '/ebtihaj.github.io/',
+    buildAssetsDir: '/_nuxt/',
+  },
+  nitro: {
+    preset: 'static'
+  },
   modules: ["@nuxt/ui"],
   compatibilityDate: "2025-02-18",
   colorMode: {
